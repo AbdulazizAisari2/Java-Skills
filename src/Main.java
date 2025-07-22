db83edba7fda1ce26f7021c8cf4895f8a7b11ed7
Abdulazizaisari2 <Abdulazizaisari06@gmail.com>
2025-07-22 14:28:40 +0400

    User info display

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args){
+		
+		UserInfo user1 = new UserInfo ("Abdulaziz",21,"Abdulazizaisari06@gmail.com", true);
+		
+		user1.displayInfo();
+		
+	}
+}
---
+++ src/UserInfo.java
@@ -0,0 +1,53 @@
+public class UserInfo {
+	//this data will be hidden(for my info)
+	private String name;
+	private int age;
+	private String email;
+	private boolean isActive;
+	
+	public UserInfo(String name, int age, String email, boolean isActive) {
+		this.name = name;
+		this.age = age;
+		this.email = email;
+		this.isActive = isActive;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	
+	public int getAge(){
+		return age;
+	}
+	public void setAge(int age){
+		this.age = age;
+	}
+	
+	
+	public String getEmail(){
+		return email;
+	}
+	public void setEmail (String email){
+		this.email = email;
+	}
+		
+
+	public boolean isActive(){
+		return isActive;
+	}
+	public void setisActive(boolean isActive){
+		this.isActive = isActive;
+	}
+	
+	public void displayInfo(){
+		System.out.println("User info:");
+		System.out.println("Name:" + " " + name);
+		System.out.println("Age:" + " " + age);
+		System.out.println("Email" + " " + email);
+		System.out.println("Active / Not Active:" + " " + isActive); 
+	}
+}
