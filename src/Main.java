e9ec20eec8d9ccca08b07c642014fd101ad4373d
Abdulazizaisari2 <Abdulazizaisari06@gmail.com>
2025-07-22 14:35:52 +0400

    Added more users (3 users)

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,12 @@ public class Main {
 	public static void main(String[] args){
 		
 		UserInfo user1 = new UserInfo ("Abdulaziz",21,"Abdulazizaisari06@gmail.com", true);
-		
+		UserInfo user2 = new UserInfo ("Mohammed",26,"Aisari1314@gmail.com", false);
+		UserInfo user3 = new UserInfo ("Ahmed",24,"AhmedAisari@gmail.com", false);
+
 		user1.displayInfo();
-		
+		user2.displayInfo();
+		user3.displayInfo();
+
 	}
 }
